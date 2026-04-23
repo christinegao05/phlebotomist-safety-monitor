d5a80b3919465c75eb0fa779706d5260edc7c6f3
Christine Gao <chrisgao@umich.edu>
2026-04-23 16:25:52 -0400

    handled boundary and endpoint failure

--- main.py
+++ main.py
@@ -4,53 +4,58 @@ import smtplib
 from email.message import EmailMessage
 import os
 from dotenv import load_dotenv
+import time
 
 # we'll add to set upon alerting, and remove when they've re-enter safe zone
 alerted = set()
+load_dotenv()
 
 def check_clinician(clinicianID):
     # gets my hidden email + pw 
-    load_dotenv()
 
     url = f"https://3qbqr98twd.execute-api.us-west-2.amazonaws.com/test/clinicianstatus/{clinicianID}"
 
-    response = requests.get(url)
-    if response.status_code != 200:
-        print(f"Could not find clinician {clinicianID}")
-        return
-    
-    data = response.json()
-    # import json
-    # print(json.dumps(data))
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Could not find clinician {clinicianID}")
+            return
+        
+        data = response.json()
+        # import json
+        # print(json.dumps(data))
 
-    # idx 0 = clinician loc
-    # idx 1 = safe range
-    clinician_loc = shape(data['features'][0]['geometry'])
-    safe_zone = shape(data['features'][1]['geometry'])
+        # idx 0 = clinician loc
+        # idx 1 = safe range
+        clinician_loc = shape(data['features'][0]['geometry'])
+        safe_zone = shape(data['features'][1]['geometry'])
 
-    # clinician is safe if their loc is within the bounding zone
-    is_safe = safe_zone.contains(clinician_loc)
+        # clinician is safe if their loc is within the bounding zone
+        is_safe = safe_zone.intersects(clinician_loc)
 
-    if not is_safe:
-        print(f"Clinician {clinicianID} is outside the safe zone.")
-        # alert if we haven't already
+        if not is_safe:
+            print(f"Clinician {clinicianID} is outside the safe zone.")
+            # alert if we haven't already
+            if clinicianID not in alerted:
+                alert(clinicianID, "is outside of safe zone")
+        else:
+            print(f"Clinician {clinicianID} is inside the safe zone.")
+            # if  previously outside safe zone, remove from alerted set
+            if clinicianID in alerted:
+                alerted.remove(clinicianID)
+    except Exception as e:
         if clinicianID not in alerted:
-            alert(clinicianID)
-    else:
-        print(f"Clinician {clinicianID} is inside the safe zone.")
-        # if  previously outside safe zone, remove from alerted set
-        if clinicianID in alerted:
-            alerted.remove(clinicianID)
+            alert(clinicianID, "endpoint did not return")
 
 
-def alert(clinicianID):
+def alert(clinicianID, reason):
     # send email
     sender = os.getenv("SENDER_EMAIL") 
     passkey = os.getenv("SENDER_PASSKEY")
-    receiver = "wwnp7b+7701g3p4vb15pnqurgo1xc33bd0@guerrillamail.info"
+    receiver = "wwpca3+2knt3o4qee243imloltrwxfw7aw@guerrillamail.info"
 
     alert_msg = EmailMessage()
-    alert_msg.set_content(f"Clinician {clinicianID} is outside of safe zone.")
+    alert_msg.set_content(f"Clinician {clinicianID} {reason}.")
     alert_msg['Subject'] = f"ALERT: Clinician in danger!"
     alert_msg['From'] = sender
     alert_msg['To'] = receiver
@@ -67,5 +72,12 @@ def alert(clinicianID):
 
 
 if __name__ == "__main__":
-    for i in range(1, 8):    
-        check_clinician(i)
+    # run this for 1 hr with 1 minute intervals
+    for minute in range (60):
+        print(f"\n--- Check {minute + 1} of {60} ({time.strftime('%H:%M:%S')}) ---")
+    
+        for i in range(1, 8):    
+            check_clinician(i)
+
+        if minute < 59:
+            time.sleep(60)
